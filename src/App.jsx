18d2f7aa6d74470e756557fb68c69376e598a98f
Victor Aragon <victoraragon@Victors-MacBook-Pro.local>
2023-05-11 00:13:50 -0400

    front end done it

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import styles from "./index.module.css";
+import sqlLogo from "./assets/sql-logo.png";
+import aiLogo from "./assets/artificial-intelligence.png";
+import { useState } from "react";
+
+function App() {
+  const [queryDescription, setQueryDescription] = useState("");
+  const [sqlQuery, setSqlQuery] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const generatedQuery = await generateQuery();
+    setSqlQuery(generatedQuery);
+  };
+
+  const generateQuery = async () => {
+    const response = await fetch("http://localhost:3005/generate", {
+      method: "POST",
+      header: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ queryDescription: queryDescription }),
+    });
+
+    const data = await response.json();
+    return data.response.trim();
+  };
+
+  return (
+    <main className={styles.main}>
+      <img src={sqlLogo} alt="" className={styles.icon} />
+      <img src={aiLogo} alt="" className={styles.icon} />
+      <h3>Generate SQL with AI</h3>
+
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="query-description"
+          placeholder="Describe your query"
+          onChange={(e) => setQueryDescription(e.target.value)}
+        />
+        <input type="submit" value="Generate query" />
+        <pre>{sqlQuery}</pre>
+      </form>
+    </main>
+  );
+}
+
+export default App;
